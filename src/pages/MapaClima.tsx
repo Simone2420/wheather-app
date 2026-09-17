033ed32a3330af6ca5f26247aea475ea5a03762c
Brian-Aldana <brian27mauricio2007@gmail.com>
2026-09-17 14:41:20 -0500

    feat(map): renderizar mapa SVG de los 33 departamentos de Colombia (Parte 2)

--- src/pages/MapaClima.tsx
+++ src/pages/MapaClima.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { departments, MAP_WIDTH, MAP_HEIGHT } from '../data/colombiaDepartments';
+import './MapaClima.css';
 
 const MapaClima: React.FC = () => {
   return (
@@ -9,8 +11,24 @@ const MapaClima: React.FC = () => {
           <IonTitle>Clima por Departamento</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className="ion-padding">
-        {/* Contenido en blanco para Checkpoint 1 */}
+      <IonContent className="ion-padding" fullscreen>
+        <div className="mapa-container">
+          <svg
+            className="mapa-svg"
+            viewBox={`0 0 ${MAP_WIDTH} ${MAP_HEIGHT}`}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {departments.map((dept) => (
+              <path
+                key={dept.name}
+                d={dept.path}
+                className="depto-path"
+              >
+                <title>{dept.name} ({dept.capital})</title>
+              </path>
+            ))}
+          </svg>
+        </div>
       </IonContent>
     </IonPage>
   );
